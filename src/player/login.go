9a7527e918372be012fd6f88044cf19103a49fa7
Yu-Cheng Liang <a135246a135246@gmail.com>
2018-10-26 19:53:29 +0800

    Add login error handling
    Login() will return `(Username, nil)` if success, or `("", err)` if
    error occurs.

--- src/player/login.go
+++ src/player/login.go
@@ -1,39 +1,49 @@
 package player
 
 import (
-    "fmt"
-    "io/ioutil"
+    "errors"
     "net/http"
+    "io/ioutil"
     "encoding/json"
 )
 
 type gitlab_user struct {
     Id int
     Username string
+    Message string
 }
 
 // Login with GitLab access token
-func Login(token string) (string, bool) {
+func Login(token string) (string, error) {
     client := &http.Client{}
 
     //TODO: use config file for hostname
     req, err := http.NewRequest("GET", "https://pd2a.imslab.org/gitlab/api/v4/user", nil)
     if err != nil {
-        fmt.Println(err.Error())
+        return "", err
     }
 
     // TODO: change to "Access-Token" for OAuth (current use private token for test)
     req.Header.Set("Private-Token", token)
 
     rsp, err := client.Do(req)
+    if err != nil {
+        return "", err
+    }
     defer rsp.Body.Close()
 
     body, err := ioutil.ReadAll(rsp.Body)
     if err != nil {
-        fmt.Println(err.Error())
+        return "", err
     }
 
     var user gitlab_user
     json.Unmarshal(body, &user)
-    return user.Username, true
+
+    // Error from GitLab server
+    if user.Message != "" {
+        return "", errors.New(user.Message)
+    }
+
+    return user.Username, nil
 }
