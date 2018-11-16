5b23eab6d7511ba560a061ff59ea63e7b4a3da12
Yu-Cheng Liang <a135246a135246@gmail.com>
2018-11-16 11:23:55 +0800

    Add LoadDefinition function
    This function can load structure definition json file.
    Current definition file is src/game/world/structures.json

--- src/game/world/structure.go
+++ src/game/world/structure.go
@@ -2,14 +2,81 @@ package world
 
 import (
     "util"
+    "encoding/json"
+)
+
+const (
+    Running = "Running"         // Running normally
+    Building = "Building"       // building or upgrading
+    Destucting = "Destucting"   // Being destruting by player
+    Destroyed = "Destroyed"     // Destroyed after war
+    Halted = "Halted"           // Halt because insufficient power
 )
 
 type Structure struct {
-    Hp      int
-    Human   int             // + for provide, - for occupy
-    Money   int             // + for produce, - for consume
-    Pos     util.Point
-    Power   int             // + for generate, - for consume
-    Size    util.Size
-    Terrain []TerrainType   // vaild construct terrain
+    ID          int
+    Name        string
+    Status      string
+
+    Human       int             // + for provide, - for occupy
+    Money       int             // + for produce, - for consume
+    Power       int             // + for generate, - for consume
+
+    Cost        int             // Money required for build
+    // Upgrade cost: 1->2 = 1 * Cost
+    //               2->3 = 2 * Cost
+    //               3->4 = 4 * Cost
+    //               4->5 = 8 * Cost
+
+    Level       int             // Building's current level
+    MaxLevel    int
+
+    Chunk       util.Point
+    Pos         util.Point
+    Size        util.Size
+
+    Terrain     int            // vaild construct terrain
+}
+
+type strProto struct {
+    ID int
+    Name string
+    Terrain []int
+    Cost int
+    Power int
+    Human int
+    Money int
+    Size uint
+}
+
+type strProtoList struct {
+    Structures []strProto
+}
+
+func LoadDefinition(structDef []byte) (structList []Structure, err error) {
+    var protoList strProtoList
+    if err = json.Unmarshal(structDef, &protoList); err != nil {
+        return
+    }
+
+    for _, s := range protoList.Structures {
+        var structure Structure
+
+        structure.ID = s.ID
+        structure.Name = s.Name
+        structure.Cost = s.Cost
+        structure.Power = s.Power
+        structure.Human = s.Human
+        structure.Money = s.Money
+
+        for _, t := range s.Terrain {
+            structure.Terrain |= t
+        }
+
+        structure.Size = util.Size { W: s.Size, H: s.Size }
+
+        structList = append(structList, structure)
+    }
+
+    return
 }
---
+++ src/game/world/terrain.go
@@ -0,0 +1,31 @@
+package world
+
+type TerrainType int
+
+// Null value is 0
+const (
+    Null TerrainType = iota
+)
+
+// Use shift for terrain value
+const (
+    Desert TerrainType = 1 << iota
+    Grass
+    Forest
+    Sea
+    River
+    Snow
+    Coast
+    Bank
+    Lava
+    Volcano
+)
+
+// Check if a terrain accepts struct's buildable terrain
+func (terrain TerrainType) Accepts(terrains int) bool {
+    if int(terrain) & terrains != 0 {
+        return true
+    }
+
+    return false
+}
